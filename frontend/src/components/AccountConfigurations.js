aa09376753ec1ba53e656bd8486e0f73341fef82
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2023-06-06 23:54:45 -0300

    feat/creating settings component

---
+++ frontend/src/components/AccountConfigurations.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const AccountConfigurations = () => {
+
+  return (
+    <div>
+        <div className="text-center bg-blue-200">
+          <p className="py-3">Minha Conta</p>
+          <p className="py-3">Configurações</p>
+          <p className="py-3">Sair</p>
+        </div>
+    </div>
+  );
+};
+
+export default AccountConfigurations;
--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -1,68 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RenderCalendar } from '../utils/Utils';
+import AccountConfigurations from './AccountConfigurations';
 import user from '../images/user.png';
 import settings from '../images/settings.png';
 
 const Calendar = (props) => {
-
   const { daysOfTheWeek, now } = props;
   const nextMonth = new Date(now.getFullYear(), now.getMonth() + 1);
   const nextMonthName = nextMonth.toLocaleString('pt-BR', { month: 'long', year: 'numeric' }).toUpperCase();
 
+  const [showAccountConfigurations, setShowAccountConfigurations] = useState(false);
+
+  const toggleAccountConfigurations = () => {
+    setShowAccountConfigurations(!showAccountConfigurations);
+  };
+  
   return (
     <div className="flex h-5/6">
-        <div className="relative flex-1 bg-white">
-            <div className="p-6 bg-gray-200 border">
-                <p className="text-center mb-3">{nextMonthName}</p>
-                <table>
-                <thead>
-                    <tr>
-                        {daysOfTheWeek.map((day) => (
-                            <th key={day} className="p-2 border text-center">
-                            {day}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                    <tbody>{RenderCalendar(nextMonth, false)}</tbody>
-                </table>
-            </div>
-            <div>
-            </div>
-            <div className="absolute bottom-0 w-full">
-                <div className="text-center" >
-                    <p className="bg-green-200 py-3">Configurações</p>
-                </div>
-                <div className="text-center">
-                    <p className="bg-blue-200 py-3">Minha Conta</p>
-                    <p className="bg-blue-200 py-3">Sair</p>
-                </div>
-                <div className="bg-white flex justify-between p-4">
-                    <img src={user} className="w-8 inline-block rounded-full" />
-                    <p className="inline-block font-bold">CRONOS AGENDA</p>
-                    <img src={settings} className="w-8 inline-block" />
-                </div>
-            </div>
+      <div className="relative flex-1 bg-white">
+        <div className="p-6 bg-gray-200 border">
+          <p className="text-center mb-3">{nextMonthName}</p>
+          <table>
+            <thead>
+              <tr>
+                {daysOfTheWeek.map((day) => (
+                  <th key={day} className="p-2 border text-center">
+                    {day}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>{RenderCalendar(nextMonth, false)}</tbody>
+          </table>
         </div>
-    
-        <div className="w-10/12 bg-blue-500">
-            <div className="w-full h-full">
-                <table className="w-full h-full">
-                    <thead>
-                        <tr>
-                            {daysOfTheWeek.map((day) => (
-                                <th key={day} className="p-2 border text-center">
-                                {day}
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                <tbody>{RenderCalendar(now, true)}</tbody>
-                </table>
-            </div>
+        <div></div>
+        <div className="absolute bottom-0 w-full">
+          {showAccountConfigurations && <AccountConfigurations />}
+          <div className="bg-white flex justify-between p-4">
+            <img src={user} className="w-8 inline-block rounded-full" alt="User" />
+            <p className="inline-block font-bold">CRONOS AGENDA</p>
+            <img
+              src={settings}
+              className="w-8 inline-block"
+              alt="Settings"
+              onClick={toggleAccountConfigurations}
+            />
+          </div>
         </div>
-  </div>
+      </div>
+      <div className="w-10/12 bg-blue-500">
+        <div className="w-full h-full">
+          <table className="w-full h-full">
+            <thead>
+              <tr>
+                {daysOfTheWeek.map((day) => (
+                  <th key={day} className="p-2 border text-center">
+                    {day}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>{RenderCalendar(now, true)}</tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   );
-}
-
+};
 export default Calendar;
